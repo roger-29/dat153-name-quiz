aecca28e9ef16084b1e635161f540891c9c11bc8
Sondre Gjellestad <sondre.gjellestad@icloud.com>
2020-02-05 13:45:35 +0100

    :art: Status bar color

--- app/src/main/java/io/roger/quiz/MainActivity.kt
+++ app/src/main/java/io/roger/quiz/MainActivity.kt
@@ -1,5 +1,6 @@
 package io.roger.quiz
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.findNavController
@@ -9,10 +10,13 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    @SuppressLint("ResourceAsColor")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setupNavigation()
+
+        window.statusBarColor = R.color.colorOnBackground
     }
 
     private fun setupNavigation() {
