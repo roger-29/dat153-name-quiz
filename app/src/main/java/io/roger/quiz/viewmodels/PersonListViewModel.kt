ca4714397f266178930bc9fa80672ec5aadc3f95
Sondre Gjellestad <sondre.gjellestad@icloud.com>
2020-02-05 09:23:31 +0100

    :sparkles: PersonListViewModel

--- app/src/main/java/io/roger/quiz/viewmodels/PersonListViewModel.kt
+++ app/src/main/java/io/roger/quiz/viewmodels/PersonListViewModel.kt
@@ -1,9 +1,27 @@
 package io.roger.quiz.viewmodels
 
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
-import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
 import io.roger.quiz.data.Person
+import io.roger.quiz.data.PersonRepository
+import io.roger.quiz.data.PersonDatabase
 
-class PersonListViewModel : ViewModel() {
-    val person = LiveData<Person>
+class PersonListViewModel(application: Application) : AndroidViewModel(application) {
+
+    private val repository: PersonRepository
+
+    val allPersons: LiveData<List<Person>>
+
+    init {
+        val personsDao = PersonDatabase.getInstance(application).personDao()
+        repository = PersonRepository(personsDao)
+        allPersons = repository.allPersons
+    }
+
+    fun insert(person: Person) = viewModelScope.launch {
+        repository.insert(person)
+    }
 }
