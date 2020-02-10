9926840ecd242fcfdc6e76742c9d75d9286d3c9c
Anders Graneng <andersgraneng@Anderss-MacBook-Pro.local>
2020-02-10 10:46:48 +0100

    Fixes on addfragment

--- app/src/main/java/io/roger/quiz/fragments/AddPersonFragment.kt
+++ app/src/main/java/io/roger/quiz/fragments/AddPersonFragment.kt
@@ -1,16 +1,27 @@
 package io.roger.quiz.fragments
 
+import android.app.Activity
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.ImageDecoder
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.RequiresApi
+import androidx.fragment.app.FragmentActivity
 import androidx.lifecycle.ViewModelProvider
 import io.roger.quiz.R
 import io.roger.quiz.data.PersonDatabase
 import io.roger.quiz.databinding.FragmentAddPersonBinding
 import io.roger.quiz.viewmodelfactories.AddViewModelFactory
 import io.roger.quiz.viewmodels.AddViewModel
+import java.lang.Exception
 
 class AddPersonFragment : Fragment() {
 
@@ -37,9 +48,29 @@ class AddPersonFragment : Fragment() {
 
         //Set onclicklistener for intent to retrieve an image
         binding.addButton.setOnClickListener {
-            activity?.let { it1 -> viewModel.getPictureIntent(it1) }
+            activity?.let { it1 -> viewModel.getPictureIntent(it1, this) }
         }
 
         return binding.root
     }
+
+    @RequiresApi(Build.VERSION_CODES.P)
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        Log.i("AddPersonFragment", "onActivityResult called")
+        if (requestCode == 1 && resultCode == FragmentActivity.RESULT_OK) {
+            Log.i("AddPersonFragment", "Image recieved")
+            val imageUri: Uri? = data?.data
+            context?.contentResolver?.let {
+                if (imageUri != null) {
+                    binding.selectedImage.setImageBitmap(ImageDecoder.decodeBitmap(ImageDecoder.createSource(it, imageUri)))
+                }
+            }
+            // val imageBitmap = data?.extras.get("data") as Bitmap ?: throw Exception("No data")
+            //val fullPhotoUri: Uri = data?.data ?: throw Exception("No Uri received")
+            //binding.selectedImage.setImageURI(fullPhotoUri)
+        }
+    }
+
+
 }
--- app/src/main/java/io/roger/quiz/viewmodels/AddViewModel.kt
+++ app/src/main/java/io/roger/quiz/viewmodels/AddViewModel.kt
@@ -2,9 +2,12 @@ package io.roger.quiz.viewmodels
 
 import android.app.Activity
 import android.app.Application
+import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.graphics.Bitmap
+import android.os.Environment
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
@@ -13,6 +16,10 @@ import androidx.lifecycle.ViewModel
 import io.roger.quiz.data.PersonDao
 import io.roger.quiz.data.PersonDatabase
 import kotlinx.coroutines.withContext
+import java.io.File
+import java.io.IOException
+import java.text.SimpleDateFormat
+import java.util.*
 
 class AddViewModel(val database: PersonDao, application: Application): AndroidViewModel(application){
 
@@ -20,17 +27,31 @@ class AddViewModel(val database: PersonDao, application: Application): AndroidVi
     val image: LiveData<Bitmap>
         get() = _image
 
-    fun getPictureIntent(activity: FragmentActivity){
+    fun getPictureIntent(activity: FragmentActivity, fragment: Fragment){
         val pictureIntent: Intent = Intent().apply {
             action = Intent.ACTION_GET_CONTENT
             type = "image/*"
         }
 
-        val chooser: Intent = Intent.createChooser(pictureIntent, "Chooser")
+        //val chooser: Intent = Intent.createChooser(pictureIntent, "Chooser")
 
         if(pictureIntent.resolveActivity(activity.packageManager) != null){
-            activity.startActivity(chooser)
+            //activity.startActivityForResult(pictureIntent, 1)
+            activity.startActivityFromFragment(fragment,pictureIntent,1)
+            //activity.startActivityForResult(chooser, 1)
         }
+    }
+
+    @Throws(IOException::class)
+    private fun createImageFile(context: Context): File{
+        //Create an image file name
+        val timeStamp: String = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
+        val storageDir: File? = context.getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        return File.createTempFile(
+            "JPEG_${timeStamp}_", //Prefix
+            ".jpg", //Suffix
+                    storageDir //Directory
+        )
     }
 
     override fun onCleared() {
